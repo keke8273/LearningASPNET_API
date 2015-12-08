9b6d4d03d894dbd0d7beea8a40520d4f9c83662f
Frank Liu <ke.frank.liu@gmail.com>
2015-12-08 14:36:15 +1100

    create a simple console client to test the web API

---
+++ LearnApsNetApi.Client/LearnApsNetApi.Client/Program.cs
@@ -0,0 +1,25 @@
+﻿using LearnApsNetApi.Common.Models;
+using System;
+using System.Net.Http;
+
+namespace LearnApsNetApi.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var httpClient = new HttpClient { BaseAddress = new Uri("http://localhost:57119") })
+            {
+                HttpResponseMessage response = httpClient.GetAsync("api/products/1").Result;
+
+                if (response.IsSuccessStatusCode)
+                {
+                    Product product = response.Content.ReadAsAsync<Product>().Result;
+                    Console.WriteLine("{0}\t${1}\t{2}", product.Name, product.Price, product.Category);
+                }
+            };
+
+            var line = Console.ReadLine();
+        }
+    }
+}
